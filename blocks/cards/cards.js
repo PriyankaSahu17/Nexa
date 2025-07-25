bd5a227cb78a4456d39155ea0a6f12172edd0e5e
Priyanka Prakash Sahu <priyankasahu@adobe.com>
2025-07-25 17:03:47 +0530

    cards added

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,24 +1,73 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
-
 export default function decorate(block) {
-  /* change to ul, li */
   const ul = document.createElement('ul');
+  ul.className = 'outer-car-box';
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    moveInstrumentation(row, li);
+    li.className = 'car-box';
     while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+
+    const children = [...li.children];
+    li.textContent = '';
+
+    let firstImageDone = false;
+    let carImageEl = null;
+    let carLogoEl = null;
+    let manualDropdownP = null;
+    children.forEach((div) => {
+      
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        if (!firstImageDone) {
+          div.className = 'car-image';
+          carImageEl = div;
+          firstImageDone = true;
+        } else if (!carLogoEl) {
+          div.className = 'car-logo';
+          carLogoEl = div;
+        }
+      } else {
+        manualDropdownP = div.querySelector('p');
+      }
     });
-    ul.append(li);
+    li.appendChild(carImageEl);
+
+    const carContentBox = document.createElement('div');
+    carContentBox.className = 'car-content';
+    carContentBox.appendChild(carLogoEl);
+
+    const manualDropdown = document.createElement('div');
+    manualDropdown.className = 'manual-dropdown';
+
+    const select = document.createElement('select');
+    select.id = `manual-dropdown`;
+    const placeholder = document.createElement('option');
+    placeholder.value = '';
+    placeholder.disabled = true;
+    placeholder.selected = true;
+    placeholder.textContent = 'Select Type';
+    select.appendChild(placeholder);
+    if (manualDropdownP) {
+      const labels = manualDropdownP.textContent
+        .split(',')
+        .map((s) => s.trim())
+        .filter(Boolean);
+      labels.forEach((label) => {
+        const opt = document.createElement('option');
+        opt.textContent = label;
+        select.appendChild(opt);
+      });
+    }
+
+    manualDropdown.appendChild(select);
+    carContentBox.appendChild(manualDropdown);
+
+    li.appendChild(carContentBox);
+    ul.appendChild(li);
   });
   ul.querySelectorAll('picture > img').forEach((img) => {
     const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
-    moveInstrumentation(img, optimizedPic.querySelector('img'));
     img.closest('picture').replaceWith(optimizedPic);
   });
-  block.textContent = '';
-  block.append(ul);
+
+  block.replaceChildren(ul);
 }
